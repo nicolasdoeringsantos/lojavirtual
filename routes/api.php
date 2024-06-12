00ca91e17dc3046a84f59a8bf592e74a5c480d3b
Clayton <claytonmagalhaes@upf.br>
2024-06-12 20:48:35 -0300

    Commit final com API

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Api\ProductsControllerApi;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('loginapi', [ProductsControllerApi::class, 'loginapi']);
+Route::get('products', [ProductsControllerApi::class, 'index'])->middleware('auth:sanctum');
